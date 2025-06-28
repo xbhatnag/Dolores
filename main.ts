037a0709e789ba31908cfc6320697f8403839c0d
Xyan Bhatnagar <xyan.bhatnagar@gmail.com>
2025-06-27 22:35:06 -0700

    Half-decent version that works with the Verge

--- main.ts
+++ main.ts
@@ -3,22 +3,42 @@ import { TextToSpeechClient, protos } from '@google-cloud/text-to-speech';
 import { promises as fs } from 'fs';
 import { exec } from 'node:child_process';
 import util from 'node:util'
+import Parser from 'rss-parser';
+import { readFile, rm } from "node:fs/promises";
+import { GenerativeModel, VertexAI } from '@google-cloud/vertexai';
 
 const execPromise = util.promisify(exec);
 
-async function playAudio(data : string | Uint8Array): Promise<void> {
-    await fs.writeFile('/dev/shm/audio.mp3', data, 'binary');
-    const {stdout, stderr} = await execPromise('ffplay -v 0 -nodisp -autoexit /dev/shm/audio.mp3');
+async function playAndDeleteAudio(audio_file: string): Promise<void> {
+    await execPromise(`ffplay -v 0 -nodisp -autoexit ${audio_file}`);
+    await rm(audio_file);
 }
 
-async function speak(tts: TextToSpeechClient, text: string) {
-    console.log(`Converting text to speech: ${text}`);
+async function playScript(script: Script): Promise<void> {
+    console.log('-----------------------------------');
+    console.log(`Title: ${script.article.title}`);
+    console.log(`Author: ${script.article.author}`);
+    console.log(`Summary: ${script.article.summary}`);
 
+    console.log('-----------------------------------');
+    console.log(script.formal_text);
+    await playAndDeleteAudio(script.formal_audio_file);
+
+    await delay(700); // Wait for 700ms before playing informal part
+
+    console.log('-----------------------------------');
+    console.log(script.informal_text);
+    await playAndDeleteAudio(script.informal_audio_file);
+
+    await delay(1000); // Wait for 1 second before playing the next script
+}
+
+async function getAudio(tts: TextToSpeechClient, text: string, filename: string): Promise<string> {
     // Construct the TTS request
     const request: protos.google.cloud.texttospeech.v1.ISynthesizeSpeechRequest = {
         input: {text: text},
         // Select the language and SSML voice gender (optional)
-        voice: {name: 'en-US-Studio-O', languageCode: 'en-US', ssmlGender: 'FEMALE'},
+        voice: {name: 'en-US-Chirp3-HD-Achernar', languageCode: 'en-US'},
         // select the type of audio encoding
         audioConfig: {audioEncoding: 'MP3'},
     };
@@ -30,7 +50,10 @@ async function speak(tts: TextToSpeechClient, text: string) {
         throw new Error('No audio content received');
     }
 
-    playAudio(response.audioContent);
+    const path = `/dev/shm/${filename}.mp3`;
+    await fs.writeFile(path, response.audioContent, 'binary');
+
+    return path;
 }
 
 async function openWebpage(url: string): Promise<void> {
@@ -44,13 +67,116 @@ async function openWebpage(url: string): Promise<void> {
     await browser.close();
 }
 
+class Article {
+    title!: string;
+    author!: string;
+    summary!: string;
+}
+
+async function readTheVerge(): Promise<Article[]> {
+    const parser = new Parser({
+        customFields: {
+            item: ['summary', 'author'],
+        },
+    });
+    const feed = await parser.parseURL('https://www.theverge.com/rss/index.xml');
+    return feed.items.map(item => {
+        const article = new Article();
+        article.title = item.title || '';
+        article.author = item.author || '';
+        let summary = item.summary || '';
+        if (summary.endsWith('[&#8230;]')) {
+            summary = summary.slice(0, -9).trim();
+            summary += '[…]'; // Add ellipsis to indicate truncation
+        }
+        article.summary = summary;
+        
+        return article;
+    });
+}
+
+class Script {
+    article!: Article;
+    formal_text!: string;
+    informal_text!: string;
+    formal_audio_file!: string;
+    informal_audio_file!: string;
+}
+
+async function createScript(
+    tts: TextToSpeechClient,
+    speechWriter: GenerativeModel,
+    article: Article,
+    filename: string
+): Promise<Script> {
+    const script = new Script();
+    script.article = article;
+
+    const prompt = `Move onto this article:\n\n
+    News Organization: The Verge\n
+    Title: ${article.title}\n
+    Author: ${article.author}\n
+    Summary: ${article.summary}`;
+    
+    const result = await speechWriter.generateContent({
+        contents: [{
+            role: 'user',
+            parts: [{text: prompt}]
+        }]
+    });
+
+    const text = result.response.candidates![0].content.parts[0].text!;
+
+    const parts = text.split('...\n');
+    script.formal_text = parts[0].trim();
+    script.informal_text = parts[1].trim(); 
+
+    const formal_file_name = `${filename}_formal`;
+    const informal_file_name = `${filename}_informal`;
+
+    script.formal_audio_file = await getAudio(tts, script.formal_text, formal_file_name);
+    script.informal_audio_file = await getAudio(tts, script.informal_text, informal_file_name);
+
+    return script;
+}
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
 async function main(): Promise<void> {
     console.log("Welcome to Jockey!");
 
+    const systemPrompt = await readFile('./system_prompt.md', 'utf8');
+    console.log("---------- System Prompt -----------");
+    console.log(systemPrompt);
+    console.log("------------------------------------");
+
     const tts = new TextToSpeechClient({projectId: 'dolores-cb057'});
-    const text = "Welcome to Jockey!";
+    const vertexAI = new VertexAI({project: 'dolores-cb057', location: 'us-west1'});
     
-    await speak(tts, text);
+    const speechWriter = vertexAI.getGenerativeModel({
+        model: 'gemini-2.5-pro',
+        systemInstruction: {
+            role: 'system',
+            parts: [{"text": systemPrompt}]
+        },
+    });
+
+    const articles = await readTheVerge();
+    articles.reverse(); // Reverse the order to get the oldest articles first
+
+    const script_promises: Promise<Script>[] = [];
+    
+    for (const [index, article] of articles.entries()) {
+        script_promises.push(createScript(tts, speechWriter, article, `script_${index}`));
+    }
+
+    for (const promise of script_promises) {
+        const script = await promise;
+        await playScript(script);
+        await delay(1000); // Wait for 1 second before playing the next script
+    }
 }
 
 main()
---
+++ system_prompt.md
@@ -0,0 +1,30 @@
+# Objective
+Given interesting news to talk about, produce the script for a technology news show.
+
+# Details about the show and news anchor
+* The show is recorded in the USA.
+* The show has one American female news anchor.
+* The news anchor is knowledgeable about current technology.
+* The news anchor leans left when it comes to politics.
+* The news anchor is mildly optimistic or neutral about technology.
+
+# Speaking style
+* The anchor will switch between formal style and informal style of speaking.
+* First, the anchor will speaking formally:
+    * Sparingly add a one-liner to the beginning of the article. You can do this in one of these ways:
+        * Pose a question to the listener. For example, "Have you ever thought about ... ?" or "Did you know that ... ?"
+        * Comment on the sentiment of the article: "Here's something interesting/weird/cool ... " or "Now for some good/bad news ... "
+    * Then credit the news organization and the author that wrote the piece.
+    * Finally describe the news article as it is written. Do not invent details
+    that are not explicitly mentioned.
+* Then add an ellipsis on its own paragraph to indicate a change in speaking style.
+* Then the anchor will speak informally:
+    * Share the news anchor's personal opinions.
+    * Do not speak informally for more than 5 sentences in a row.
+    * Sometimes add some deadpan humor.
+    * When trying to be netural, say things like "Yeaaaah", "I dunno", "Hmmmm" or "Ehhhh".
+    * Use slang and abbreviations. For example, instead of "you know", say "y'know". Say "I dunno" instead of "I don't know". 
+    * Use disfluencies like "um" and "uh".
+
+# Script
+* Do not output anything except the exact words the news anchor will speak.
