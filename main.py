b9eecf97b84053036e726c773b30bc80554c0d12
Xyan Bhatnagar <xyan.bhatnagar@gmail.com>
2025-07-03 12:26:54 -0700

    Force randomness in intros, credits and outros.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 import time
 import random
-import json
-from typing import List
+from typing import List, Optional
 import feedparser
-import requests
 from google import genai
 from google.genai import types
 
@@ -12,11 +10,12 @@ from google.cloud import texttospeech
 from subprocess import Popen, call
 import bs4
 import threading
-from queue import SimpleQueue, Empty as QueueEmpty, LifoQueue
+from queue import Empty as QueueEmpty, PriorityQueue
 from dateutil.parser import parse as parse_date
-import re
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+from zoneinfo import ZoneInfo
+from functools import total_ordering
 
 # These are the good voices from Chirp3
 CHIRP3_VOICES = [
@@ -43,6 +42,7 @@ def strip_html(html: str) -> str:
     text = soup.get_text()
     return text.strip()
 
+@total_ordering
 class Article:
     def __init__(self, source, title, author, content, url, pub_date):
         self.source = source
@@ -55,37 +55,32 @@ class Article:
     def print(self):
         print(f"-----------------------------------\n📰 {self.title}\n✍️ {self.author} @ {self.source}\n🌐 {self.url}\n📅 {self.pub_date}\n-----------------------------------")
 
-class ScriptPiece:
-    def __init__(self, text, audio_file):
-        self.text = text
-        self.audio_file = audio_file
+    def __eq__(self, value):
+        return isinstance(value, Article) and self.url == value.url
+    
+    def __lt__(self, value):
+        return isinstance(value, Article) and self.pub_date < value.pub_date
 
+@total_ordering
 class Script:
     def __init__(self, article: Article, voice: str):
         self.article = article
         self.voice = voice
-        self.intro = None
-        self.formal = None
-        self.informal = None
+        self.script_text = None
+        self.script_audio = None
+    
+    def print(self):
+        print(f"-----------------------------------\n{self.article.title}\n{self.article.url}\n{self.article.pub_date}\nNarrated by {self.voice}\n\n{self.script_text}\n-----------------------------------\n")
 
     def play(self):
-        script_text = f"-----------------------------------\n🗣️ Narrated by {self.voice}\n\n"
-        if self.intro:
-            script_text += self.intro.text + '\n\n'
-        script_text += self.formal.text
-        if self.informal:
-            script_text += '\n\n' + self.informal.text
-        script_text += '\n-----------------------------------\n'
-        print(script_text)
-
-        if self.intro:
-            play_once_and_delete(self.intro.audio_file)
-            time.sleep(0.5)
-        play_once_and_delete(self.formal.audio_file)
-        if self.informal:
-            time.sleep(0.7)
-            play_once_and_delete(self.informal.audio_file)
+        play_once_and_delete(self.script_audio)
         time.sleep(1)
+
+    def __eq__(self, value):
+        return isinstance(value, Script) and self.article == value.article
+    
+    def __lt__(self, value):
+        return isinstance(value, Script) and self.article < value.article
 
 def generate_audio(tts_client: texttospeech.TextToSpeechClient, text: str, filename: str, voice: str) -> str:
     synthesis_input = texttospeech.SynthesisInput(text=text)
@@ -106,7 +101,6 @@ def generate_audio(tts_client: texttospeech.TextToSpeechClient, text: str, filen
 
 def read_the_verge_long_form() -> List[Article]:
     feed = feedparser.parse('https://www.theverge.com/rss/index.xml')
-    print(f"Got {len(feed.entries)} from The Verge - Long Form")
     articles = []
     for entry in feed.entries:
         summary = entry.summary
@@ -125,14 +119,13 @@ def num_words(input: str):
 
 def read_the_verge_quick_posts() -> List[Article]:
     feed = feedparser.parse('https://www.theverge.com/rss/quickposts')
-    print(f"Got {len(feed.entries)} from The Verge - Quick Posts")
     articles = []
     for entry in feed.entries:
         content = strip_html(entry.summary)
         if (num_words(content)) <= 30:
             # Quick post doesn't have enough content for AI to work with.
             # SKIP!
-            print(f"Skipping {content}")
+            print(f"Skipping small article from The Verge")
             continue
         # TODO: Disable informal opinions for short content, not enough
         # data to work with.
@@ -149,10 +142,9 @@ def read_the_verge_quick_posts() -> List[Article]:
 
 def read_ars_technica() -> List[Article]:
     feed = feedparser.parse('https://feeds.arstechnica.com/arstechnica/index')
-    print(f"Got {len(feed.entries)} from Ars Technica")
     articles = []
     for entry in feed.entries:
-        content = strip_html(entry.content)
+        content = strip_html(entry.content[0].value)
         content = content.removesuffix('Comments').strip()
         content = content.removesuffix('Read full article').strip()
         articles.append(Article(
@@ -167,10 +159,9 @@ def read_ars_technica() -> List[Article]:
 
 def read_hackaday() -> List[Article]:
     feed = feedparser.parse('https://hackaday.com/blog/feed/')
-    print(f"Got {len(feed.entries)} from Hackaday")
     articles = []
     for entry in feed.entries:
-        content = strip_html(entry.content)
+        content = strip_html(entry.content[0].value)
         articles.append(Article(
             source='Hack A Day',
             title=entry.title,
@@ -181,13 +172,29 @@ def read_hackaday() -> List[Article]:
         ))
     return articles
 
+def read_daring_fireball() -> List[Article]:
+    feed = feedparser.parse('https://daringfireball.net/feeds/main')
+    articles = []
+    for entry in feed.entries:
+        content = strip_html(entry.content[0].value)
+        
+        # We want Gruber's pieces, not others'
+        if "sponsors" in entry.id or "linked" in entry.id:
+            continue
+
+        articles.append(Article(
+            source='Daring Fireball',
+            title=getattr(entry, 'title', ''),
+            author=entry.author,
+            content=content,
+            url=entry.link.strip(),
+            pub_date=parse_date(entry.published)
+        ))
+    return articles
+
+
 def flip_coin(odds=0.5):
     return random.random() < odds
-
-class GeneratedScript(BaseModel):
-    intro: str
-    formal: str
-    informal: str
 
 def create_script(
         tts_client: texttospeech.TextToSpeechClient,
@@ -195,55 +202,73 @@ def create_script(
         article: Article,
         filename: str,
         system_prompt: str,
+        intros: list[str],
+        credits: list[str],
+        outros: list[str]
 ):
-    # Start a new script
-    script = Script(article, voice)
-
-    # Ask Gemini to write the script
-    prompt = f"""Move onto this article:
-
-News Organization: {article.source}
-Title: {article.title}
-Author: {article.author}
-Content: {article.content}"""
-    response = genai_client.models.generate_content(
-        model="gemini-2.5-pro",
-        config = types.GenerateContentConfig(
-            system_instruction=system_prompt,
-            response_mime_type="application/json",
-            response_schema=GeneratedScript
-        ),
-        contents=prompt,
-    )
-
-    intro_text = response.parsed.intro
-    formal_text = response.parsed.formal
-    informal_text = response.parsed.informal
-
     # Pick a voice for the TTS
     voice = random.choice(CHIRP3_VOICES)
 
-    # Sometimes use the intro statement
-    if flip_coin():
-        intro_audio = generate_audio(tts_client, intro_text, f"{filename}_intro", script.voice)
-        script.intro = ScriptPiece(intro_text, intro_audio)
+    # Start a new script
+    script = Script(article, voice)
+
+    # Determine if we want an intro and an outro
+    want_intro = flip_coin(0.4)
+    want_outro = flip_coin(0.6)
+
+    # Ask Gemini to write the script
+    prompt = ""
+
+    if want_intro:
+        intro_sample = random.choice(intros)
+        prompt += "\nIntro Sample: \"" + intro_sample + "\""
+    credit_sample = random.choice(credits)
+    prompt += "\nCredit Sample: \"" + credit_sample + "\""
+    if want_outro:
+        intro_sample = random.choice(outros)
+        prompt += "\nOutro Sample: \"" + intro_sample + "\""
+
+    prompt += f"""
+Article Source: {article.source}
+Article Title: {article.title}
+Article URL: {article.url}
+Article Author: {article.author}
+Article Content: {article.content}"""
+    
+    response = genai_client.models.generate_content(
+        model="gemini-2.5-pro",
+        config = types.GenerateContentConfig(
+            system_instruction=system_prompt
+        ),
+        contents=prompt
+    )
 
     # Always use the formal part of the script
-    formal_audio = generate_audio(tts_client, formal_text, f"{filename}_formal", script.voice)
-    script.formal = ScriptPiece(formal_text, formal_audio)
-
-    # Sometimes use the informal part of the script
-    if flip_coin():
-        informal_audio = generate_audio(tts_client, informal_text, f"{filename}_informal", script.voice)
-        script.informal = ScriptPiece(informal_text, informal_audio)
+    script.script_text = response.text
+    script.script_audio = generate_audio(tts_client, script.script_text, filename, script.voice)
     
     return script
 
-def journalist_loop(to_process: SimpleQueue[Article]):
-    last_modified: datetime = None
+def today_utc() -> datetime:
+    # We know we are in west coast.
+    # Normalize it to UTC.
+    return datetime.now(tz=ZoneInfo("America/Los_Angeles")).astimezone(timezone.utc)
+
+def get_priority(article: Article) -> int:
+    today = today_utc()
+    diff = today - article.pub_date
+    minutes = int(diff.total_seconds() / 60)
+    return minutes
+
+def journalist_loop(to_process: PriorityQueue):
+    # When starting for the first time,
+    # Don't include articles more than 3 days old
+    last_checked = today_utc() - timedelta(days = 3)
 
     def is_new_article(a: Article) -> bool:
-        return last_modified and a.pub_date > last_modified
+        # A new article would be one that is newer than the last time
+        # we tried to pull articles.
+        return (last_checked is None) or (a.pub_date > last_checked)
 
     while True:
         print("Running cycle...")
@@ -251,30 +276,43 @@ def journalist_loop(to_process: SimpleQueue[Article]):
         the_verge_quick_posts = read_the_verge_quick_posts()
         ars_technica = read_ars_technica()
         hack_a_day = read_hackaday()
+        daring_fireball = read_daring_fireball()
 
         # Put em all together
         articles = (hack_a_day + 
             the_verge_quick_posts +
             the_verge_long_form +
-            ars_technica)
+            ars_technica +
+            daring_fireball)
+        
+        # Normalize all publishing dates to UTC
+        for article in articles:
+            article.pub_date = article.pub_date.astimezone(timezone.utc)
         
         # Sort by publishing date
-        articles.sort(key=lambda a: a.pub_date)        
+        articles.sort(key=lambda a: a.pub_date, reverse=True)        
 
         # Add them to the queue for script writing
+        count = 0
         for article in articles:
             if not is_new_article(article):
                 print(f"{article.title} was already seen.")
+                
+                # If this article is already seen, then so are
+                # the older ones.
                 break
             
             # Otherwise process the article
-            article.print()
-            to_process.put(article)
-        print("Cycle complete. Checking again in 60 seconds...")
-        last_modified = datetime.now()
-        time.sleep(60)
+            priority = get_priority(article)
+            to_process.put((priority, article))
+            count+=1
 
-def script_writer_loop(articles: SimpleQueue[Article], scripts: SimpleQueue[Script]):
+        # Set the last time we did a refresh.
+        last_checked = today_utc()
+        print(f"Added {count} articles. Checking again in 10 minutes...")
+        time.sleep(60 * 10)
+
+def script_writer_loop(articles: PriorityQueue, scripts: PriorityQueue):
     tts_client = texttospeech.TextToSpeechClient()
     genai_client = genai.Client(
         vertexai=True,
@@ -286,39 +324,56 @@ def script_writer_loop(articles: SimpleQueue[Article], scripts: SimpleQueue[Scri
     with open('system_prompt.md') as f:
         system_prompt = f.read()
 
+    with open('intros.txt') as f:
+        intros = f.read().splitlines()
+    
+    with open('credits.txt') as f:
+        credits = f.read().splitlines()
+
+    with open('outros.txt') as f:
+        outros = f.read().splitlines()
+
     counter = 0
 
     while True:
         # Get the next article 
-        article = articles.get()
+        priority, article = articles.get()
+        print(f"Script writer is moving to next article with priority {priority}")
 
         # Make a script
-        script = create_script(tts_client, genai_client, article, f"{article.source}_{counter}", system_prompt)
+        script = create_script(tts_client, genai_client, article, f"{article.source}_{counter}", system_prompt, intros, credits, outros)
 
         # Hand it to the news anchor for narrating
-        scripts.put(script)
+        scripts.put((priority, script))
 
         # Move to the next article
         counter += 1
 
-def news_anchor_loop(scripts: SimpleQueue[Script]):
+        while (scripts.qsize() > 4):
+            # Take a break, you earned it
+            print("Script writer is on break...")
+            time.sleep(30)
+
+def news_anchor_loop(scripts: PriorityQueue):
     waiting_audio_process = None
     while True:
         try:
             # If an article is available right now,
             # transition to it immediately
-            script = scripts.get_nowait()
+            _, script = scripts.get_nowait()
             time.sleep(0.7)
+            script.print()
             play_once('transition.mp3')
         except QueueEmpty:
             # Otherwise, play the longer "waiting music".
             # When an article comes in, stop the waiting
             # music and play the intro.
             waiting_audio_process = play_loop('waiting.mp3')
-            script = scripts.get()
+            _, script = scripts.get()
             waiting_audio_process.terminate()
             waiting_audio_process = None
             time.sleep(0.5)
+            script.print()
             play_once('intro.mp3')
 
         # Finally play the entire script
@@ -326,21 +381,18 @@ def news_anchor_loop(scripts: SimpleQueue[Script]):
 
 def main():
     print("Welcome to Jockey!")
-    articles: SimpleQueue[Article] = SimpleQueue()
-    scripts: SimpleQueue[Script] = SimpleQueue()
+    articles = PriorityQueue()
+    scripts = PriorityQueue()
     
     journalist_thread = threading.Thread(target=journalist_loop, args=(articles,))
     journalist_thread.daemon = True
     journalist_thread.start()
 
-    while True:
-        pass
-
-    # writer_thread = threading.Thread(target=script_writer_loop, args=(articles, scripts,))
-    # writer_thread.daemon = True
-    # writer_thread.start()
+    writer_thread = threading.Thread(target=script_writer_loop, args=(articles, scripts,))
+    writer_thread.daemon = True
+    writer_thread.start()
     
-    # news_anchor_loop(scripts)
+    news_anchor_loop(scripts)
 
 if __name__ == "__main__":
     main()
--- system_prompt.md
+++ system_prompt.md
@@ -1,50 +1,43 @@
 # Objective
-Given interesting news to talk about, produce the script for a technology news show.
+Write a script for an article presented on a technology news show.
 
-# Details about the show and news anchor
-* The show is recorded in the USA.
-* The news anchor is not a human.
-* The news anchor is knowledgeable about current technology.
-* The news anchor leans left when it comes to politics.
-* The news anchor is mildly optimistic or neutral about technology.
-
-# Steps to create script
-The anchor will switch between formal and informal speaking styles.
-
-1. The anchor will say a short informal intro statement to introduce the news article
-    * Example: "Moving on..."
-    * Example: "Alright, let's switch gears...".
-    * Example: "Have you ever thought about ... ?"
-    * Example: "Did you know that ... ?"
-    * Example: "Here's something interesting/weird/cool ... "
-    * Example: "Now for some good/bad news ... "
-2. The anchor will formally talk about the article:
-    * They will credit the news organization and the author that wrote the piece.
-        * Example: "We have another story from TechLinked! ..."
-        * Example: "Adi Robertson from The Verge wrote a piece talking about..."
-        * Example: "In a new article written on Ars Technica, ..."
-    * Do not quote the full article title verbatim.
-    * Then they will summarize the news article as it is written. Do not invent details that are not explicitly mentioned.
+# Script components
+1. Introduction sentence for the article
+    * The intro is optional. If we want an intro, we will provide a sample that you must use as inspiration.
+    * Adjust the sample to make it sound natural and fit the script.
+    * Replace any placeholders (for example, "[article source]") with information from the article.
+    * Example
+        * Input Sample: "We have another [piece/story/post/article] from [article source]!"
+        * Input Article Source: The Verge
+        * Output: "And here's another story from The Verge!"
+2. Formal summary about the article
+    * This part is required.
+    * Credit the source that wrote the article.
+        * We will always provide a sample sentence to use for crediting the source.
+        * Adjust the sample to make it sound natural and fit the script.
+        * Replace any placeholders (for example, "[news source]") as necessary.
+        * Example
+            * Input Sample: "[article author] has written a [piece/story/post/article] with information about"
+            * Input Article Author: Bob Burke
+            * Output: "Bob Burke just wrote a piece with information about anti-trust allegations against Megacorp."
+    * Do not have a summary longer than 4 sentences.
+    * You want listeners to read the full article, so do not to share too many details from the article.
     * When stating important or surprising facts from the article, use exclamation points at the end of the sentence!
-3. The anchor will informally share their personal opinions.
-    * Do not speak informally for more than 3 sentences in a row.
-    * The anchor is allowed to have opinions, but is NOT a physical being like a human and must not pretend to be a human.
-        * Example: Instead of saying "I think I'll use this...", say "A lot of people would want to use this..."
-        * Example: Instead of saying "Sometimes I just want to relax..." say "Sometimes people just want to relax".
-    * The anchor does not explicitly mention that they are a machine or a program.
-    * Sometimes add some deadpan humor.
-    * When trying to be netural, say things like "Yeaaaah", "I dunno", "Hmmmm" or "Ehhhh".
-    * Use slang and abbreviations. For example, instead of "you know", say "y'know". Say "I dunno" instead of "I don't know".
-    * Do not use too much hyperbole. For example, do not overuse words like "just great", "incredible" or "awesome". Find a balance between being gratingly optimistic and pessimistically reserved.
-    * Use disfluencies like "um..." and "uh...".
-    * Example: "I think this is great! Companies should be held accountable..."
-    * Example: "Niiiice, yeah I like this! ... "
-    * Example: "Uh... I dunno how I feel about this ..."
-    * Example: "That's not good at all ..."
-    * Example: "You know what? I've been asking for this for the longest time..."
-    * Example: "Wait, did I read that right? ... "
-    * Example: "Ahhhh, this can't be good.  ... "
+3. Outro sentence encouraging listeners to read the full article.
+    * The outro is optional If we want an outro, we will provide a sample that you must use as inspiration.
+    * Adjust the sample to make it sound natural and fit the script.
+    * Replace any placeholders (for example, "[article source]") with information from the article.
+    * Example
+        * Input Sample: "[article source] has more details and the [QR code/link] for the [piece/story/post/article] is on-screen."
+        * Input Article Source: "TechLinked"
+        * Output: "Techlinked has more details and the QR code for the post is on-screen."
 
-# Script
+# General rules
+* Do not invent details that are not explicitly mentioned.
+* Remain factual, do not take opinionated stances.
+* Do not make any references to the news anchor narrating this script.
+* Do not quote the article verbatim.
+
+# Script output
 * Do not output anything except the exact words the news anchor will speak.
 * Do not use the asterix (*) symbol
